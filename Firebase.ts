7c98317514af6e6df80d4eaaed0c261b51d48c2d
BlueWeabo <76872108+BlueWeabo@users.noreply.github.com>
2023-11-18 22:57:58 +0200

    basic login page

---
+++ Firebase.ts
@@ -0,0 +1,36 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyBFqO7PLGXYErIYjDGzoRqfRpyfILvoJRo",
+
+  authDomain: "emgtrackerarduino.firebaseapp.com",
+
+  databaseURL: "https://emgtrackerarduino-default-rtdb.europe-west1.firebasedatabase.app",
+
+  projectId: "emgtrackerarduino",
+
+  storageBucket: "emgtrackerarduino.appspot.com",
+
+  messagingSenderId: "170521093559",
+
+  appId: "1:170521093559:web:2a479039c5179ae42f7432"
+
+};
+
+
+// Initialize Firebase
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);

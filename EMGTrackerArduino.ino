7e4ede3e716eac4ceeed98fbdec7c4c95343a310
BlueWeabo <76872108+BlueWeabo@users.noreply.github.com>
2023-11-28 20:28:21 +0200

    fix arduino code

--- EMGTrackerArduino.ino
+++ EMGTrackerArduino.ino
@@ -1,5 +1,3 @@
-#include <Vector.h>
-
 #include <WiFi.h>
 #include <Firebase.h>
 #include "time.h"
@@ -9,32 +7,30 @@
 #define DATABASE_SECRET "OAUHcMrInFfygL6UHISgzwd4bsOajW7XYepeNfWR"
 #define MONDAY "Monday"
 #define SUNDAY "Sunday"
-// Define the Firebase Data object
+// Define the Firebase data object
 FirebaseData fbdo;
 
-// Define the FirebaseAuth data for authentication data
+// Define the FirebaseAuth val for authentication val
 FirebaseAuth auth;
 
-// Define the FirebaseConfig data for config data
+// Define the FirebaseConfig val for config val
 FirebaseConfig config;
 
 const char* ntpServer = "pool.ntp.org";
 const long gmtOffset_sec = 7200;
-const int daylightOffset_sec = 3600;
+const int daylightOffset_sec = 0;
 
-const char* CLIENT = "/Muscle Biometrics/Ilia Iliev/Current";
-const char* CURRENT = "Current";
-const char* MONDAY_HOUR2 = "Monday/Hour2";
-const char* MONDAY_HOUR10 = "Monday/Hour10";
-const char* MONDAY_HOUR17 = "Monday/Hour17";
-const char* SUNDAY_HOUR2 = "Sunday/Hour2";
-const char* SUNDAY_HOUR10 = "Sunday/Hour10";
-const char* SUNDAY_HOUR17 = "Sunday/Hour17";
+const String CLIENT = "/Muscle Biometrics/Ilia Iliev/Current";
+const String MONDAY_HOUR2 = "/Muscle Biometrics/Ilia Iliev/Monday/Hour2/";
+const String MONDAY_HOUR10 = "/Muscle Biometrics/Ilia Iliev/Monday/Hour10/";
+const String MONDAY_HOUR17 = "/Muscle Biometrics/Ilia Iliev/Monday/Hour17/";
+const String SUNDAY_HOUR2 = "/Muscle Biometrics/Ilia Iliev/Sunday/Hour2/";
+const String SUNDAY_HOUR10 = "/Muscle Biometrics/Ilia Iliev/Sunday/Hour10/";
+const String SUNDAY_HOUR17 = "/Muscle Biometrics/Ilia Iliev/Sunday/Hour17/";
 char timeWeekDay[10];
 char timeMin[3];
 char timeHour[3];
-int EMGSensor;
-int* bigData;
+int EMGSensor = 21;
 void setup() {
   Serial.begin(9600);
   WiFi.begin(WIFI_NAME, WIFI_PASSWORD);
@@ -56,7 +52,7 @@ void setup() {
   Firebase.reconnectNetwork(true);
 
   // Since v4.4.x, BearSSL engine was used, the SSL buffer need to be set.
-  // Large data transmission may require larger RX buffer, otherwise connection issue or data read time out can be occurred.
+  // Large val transmission may require larger RX buffer, otherwise connection issue or val read time out can be occurred.
   fbdo.setBSSLBufferSize(4096 /* Rx buffer size in bytes from 512 - 16384 */, 1024 /* Tx buffer size in bytes from 512 - 16384 */);
 
   /* Initialize the library with the Firebase authen and config */
@@ -67,59 +63,58 @@ void setup() {
 }
 boolean start = false;
 boolean done = false;
+long cycle = 0;
 void loop() {
-  int val = random(1024); //analogRead(EMGSensor);
+  int val = analogRead(EMGSensor);
   //char thing[] = CLIENT + CURRENT;
   Firebase.set(fbdo, CLIENT, val);
   printLocalTime();
-//  if (timeWeekDay == MONDAY) {
-//    if ((timeHour == "2" || timeHour == "10" || timeHour == "17") && !done) {
-//      start= true;
-//    }
-//    if ((timeHour == "3" || timeHour == "11" || timeHour == "18") && done) {
-//      start = false;
-//      done = false;
-//    }
-//  }
-//  if (timeWeekDay == SUNDAY) {
-//    if ((timeHour == "2" || timeHour == "10" || timeHour == "17") && !done) {
-//      start= true;
-//    }
-//    if ((timeHour == "3" || timeHour == "11" || timeHour == "18") && done) {
-//      start = false;
-//      done = false;
-//    }
-//  }
-//  if (start && !done) {
-//    if (timeMin == "1" || timeMin == "2") {
-//      char magic[];
-//      if (timeWeekDay == MONDAY) {
-//        if (timeHour == "2") {
-//          magic = CLIENT + MONDAY_HOUR2;
-//        }
-//        if (timeHour == "10") {
-//          magic = CLIENT + MONDAY_HOUR10;
-//        }
-//        if (timeHour == "17") {
-//          magic = CLIENT + MONDAY_HOUR17;
-//        }
-//      }
-//      if (timeWeekDay == SUNDAY) {
-//        if (timeHour == "2") {
-//          magic = CLIENT + SUNDAY_HOUR2;
-//        }
-//        if (timeHour == "10") {
-//          magic = CLIENT + SUNDAY_HOUR10;
-//        }
-//        if (timeHour == "17") {
-//          magic = CLIENT + SUNDAY_HOUR17;
-//        }
-//      }
-//      Firebase.set(fbdo, magic, *bigData);
-//      done = true;
-//    }
-//    *bigData += val;
-//  }
+  if (timeWeekDay == MONDAY) {
+    if ((timeHour == "2" || timeHour == "10" || timeHour == "17") && !done) {
+      start= true;
+    }
+    if ((timeHour == "3" || timeHour == "11" || timeHour == "18") && done) {
+      start = false;
+      done = false;
+      cycle = 0;
+    }
+  }
+  if (timeWeekDay == SUNDAY) {
+    if ((timeHour == "2" || timeHour == "10" || timeHour == "17") && !done) {
+      start= true;
+    }
+    if ((timeHour == "3" || timeHour == "11" || timeHour == "18") && done) {
+      start = false;
+      done = false;
+    }
+  }
+  if (start && !done) {
+    if (timeMin == "1" || timeMin == "2") {
+      done = true;
+    }
+    if (timeWeekDay == MONDAY) {
+      if (timeHour == "2") {
+        Firebase.set(fbdo, MONDAY_HOUR2 + String(cycle), val);
+      }
+      if (timeHour == "10") {
+        Firebase.set(fbdo, MONDAY_HOUR10 + String(cycle), val);
+      }
+      if (timeHour == "17") {
+        Firebase.set(fbdo, MONDAY_HOUR17 + String(cycle), val);
+      }
+    }
+    if (timeWeekDay == SUNDAY) {
+      if (timeHour == "2") {
+        Firebase.set(fbdo, SUNDAY_HOUR2 + String(cycle), val);
+      }
+      if (timeHour == "10") {
+        Firebase.set(fbdo, SUNDAY_HOUR10 + String(cycle), val);
+      }
+      if (timeHour == "17") {
+        Firebase.set(fbdo, SUNDAY_HOUR17 + String(cycle), val);
+      }
+    }
+  }
 }
 void printLocalTime(){
   struct tm timeinfo;
